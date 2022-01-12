333a6fc0b77b2f5147a2036be6d4eb1d40a50f42
Viktor Kassov <kassov@mail.ua>
2022-01-12 11:39:57 +0200

    5/5+

--- Unit Converter/task/src/converter/Main.kt
+++ Unit Converter/task/src/converter/Main.kt
@@ -1,6 +1,40 @@
 package converter
 
 import java.util.*
+enum class TemperatureMeasureUnit(val nameList: MutableList<String>) {
+    Celsius(mutableListOf("c", "degree Celsius", "degrees Celsius", "celsius", "dc")),
+    Fahrenheit(mutableListOf("f", "degree Fahrenheit", "degrees Fahrenheit", "fahrenheit", "df")),
+    Kelvins(mutableListOf("k", "kelvin", "kelvins"));
+    companion object {
+        fun Convert(v: Double, f1: TemperatureMeasureUnit,f2: TemperatureMeasureUnit): Double {
+            return when {
+                f1 == f2 -> v
+                f1 == Celsius && f2 == Fahrenheit -> (v * 9 / 5) + 32
+                f2 == Celsius && f1 == Fahrenheit -> (v - 32) * 5 / 9
+                f1 == Kelvins && f2 == Fahrenheit -> (v * 9 / 5) - 459.67
+                f2 == Kelvins && f1 == Fahrenheit -> (v + 459.67) * 5 / 9
+                f1 == Celsius && f2 == Kelvins -> v + 273.15
+                f2 == Celsius && f1 == Kelvins -> v - 273.15
+                else -> 0.0
+            }
+        }
+        fun ShowConvertResult(v: Double, f1: TemperatureMeasureUnit,f2: TemperatureMeasureUnit) {
+            val m1: String = if (v == 1.0) {
+                f1.nameList[1]
+            } else {
+                f1.nameList[2]
+            }
+            val v2 = Convert(v, f1, f2)
+            val m2: String = if (v2 == 1.0) {
+                f2.nameList[1]
+            } else {
+                f2.nameList[2]
+            }
+            println("$v $m1 is $v2 $m2")
+        }
+    }
+}
+
 enum class LengthMeasureUnit (val nameList: MutableList<String>, val k: Double) {
     Meter(mutableListOf("m", "meter", "meters"), 1.0),
     Kilometer(mutableListOf("km", "kilometer","kilometers"), 1000.0),
@@ -59,34 +93,54 @@ fun main() {
             "exit" -> return
             else -> {
                 try {
-                    val p = input.split(" ")
+                    val delim = if (input.contains(" to ")) " to " else if (input.contains(" in ")) " in " else " convertto "
+                    val tmp = input.split(delim)
+                    val p = mutableListOf<String>()
+                    tmp[0].trim().split("\\s".toRegex(), 2).forEach { p.add(it) }
+                    p.add(delim)
+                    p.add(tmp[1].trim())
                     val v = p[0].toDouble()
-                    val lFrom = LengthMeasureUnit.values().filter { it.nameList.contains(p[1]) }
-                    val lTo = LengthMeasureUnit.values().filter { it.nameList.contains(p[3]) }
-                    val wFrom = WeightMeasureUnit.values().filter { it.nameList.contains(p[1]) }
-                    val wTo = WeightMeasureUnit.values().filter { it.nameList.contains(p[3]) }
+                    val lFrom = LengthMeasureUnit.values().filter { it.nameList.map { it.lowercase(Locale.getDefault()) }.contains(p[1])  }
+                    val lTo = LengthMeasureUnit.values().filter { it.nameList.map { it.lowercase(Locale.getDefault()) }.contains(p[3]) }
+                    val wFrom = WeightMeasureUnit.values().filter { it.nameList.map { it.lowercase(Locale.getDefault()) }.contains(p[1]) }
+                    val wTo = WeightMeasureUnit.values().filter { it.nameList.map { it.lowercase(Locale.getDefault()) }.contains(p[3]) }
+                    val tFrom = TemperatureMeasureUnit.values().filter { it.nameList.map { it.lowercase(Locale.getDefault()) }.contains(p[1]) }
+                    val tTo = TemperatureMeasureUnit.values().filter { it.nameList.map { it.lowercase(Locale.getDefault()) }.contains(p[3]) }
                     if (lFrom.isNotEmpty() && lTo.isNotEmpty()) {
-                        lFrom[0].ShowConvertResult(v, lTo[0])
+                        if (v < 0) {
+                            println("Length shouldn't be negative")
+                        } else {
+                            lFrom[0].ShowConvertResult(v, lTo[0])
+                        }
                     } else if (wFrom.isNotEmpty() && wTo.isNotEmpty()) {
-                        wFrom[0].ShowConvertResult(v, wTo[0])
+                        if (v < 0) {
+                            println("Weight shouldn't be negative")
+                        } else {
+                            wFrom[0].ShowConvertResult(v, wTo[0])
+                        }
+                    } else if (tFrom.isNotEmpty() && tTo.isNotEmpty())  {
+                        TemperatureMeasureUnit.ShowConvertResult(v, tFrom[0], tTo[0])
                     } else {
                         val from = when {
                             lFrom.isNotEmpty() -> lFrom[0].nameList[2]
                             wFrom.isNotEmpty() -> wFrom[0].nameList[2]
+                            tFrom.isNotEmpty() -> tFrom[0].nameList[2]
                             else -> "???"
                         }
                         val to = when {
                             lTo.isNotEmpty() -> lTo[0].nameList[2]
                             wTo.isNotEmpty() -> wTo[0].nameList[2]
+                            tTo.isNotEmpty() -> tTo[0].nameList[2]
                             else -> "???"
                         }
                         println("Conversion from $from to $to is impossible")
                     }
                 } catch (e: Exception) {
-                    println("Wrong Input")
+                    println("Parse error")
                 }
             }
         }
+        println()
     }
 }
 
